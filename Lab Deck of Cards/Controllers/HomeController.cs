57937261e4ade07d828a70e1af04dee8c56aa9bc
matthewfoxmi <matthewfoxmi@gmail.com>
2022-07-25 17:27:28 -0400

    Turning In

---
+++ Lab Deck of Cards/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Lab_Deck_of_Cards.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab_Deck_of_Cards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CardsDAL cardsDAL = new CardsDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CardDisplay(int cards)
+        {            
+            CardsModel result = new CardsModel();
+            if (cards >= result.remaining)
+            {
+                result = cardsDAL.DrawCards(cards);
+            }
+            else
+            {
+                cardsDAL.ShuffleCards();
+                Console.WriteLine("You don't have enough cards left, re-shuffling now");
+            }
+
+            return View(result);
+        }
+
+        public IActionResult Draw()
+        {
+            return View();
+        }   
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab Deck of Cards/Models/CardsDAL.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Lab_Deck_of_Cards.Models
+{
+    public class CardsDAL
+    {
+        public CardsModel DrawCards(int cards) 
+        {                                                                                                                   
+            string url = $"https://deckofcardsapi.com/api/deck/6mcu3tye8pd3/draw/?count={cards}";
+
+            //setup request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //capturing response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //save all of this response data as a variable
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd(); 
+
+            //convert JSON to a C# object
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+
+            return result;
+        }
+
+        public void ShuffleCards()
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/6mcu3tye8pd3/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            
+        }
+    }
+}
